3766e389f1d3678a97c0e6cc8344f4c61a142925
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-09-15 20:44:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // for local dev if needed
-    port: 5173,
-  },
   preview: {
-    host: "0.0.0.0", // required for Render
-    port: process.env.PORT || 10000, // use Render's assigned port
+    host: "0.0.0.0",
+    port: process.env.PORT || 10000,
+    allowedHosts: ["task-weather-app-7f0y.onrender.com"]
   },
 });
